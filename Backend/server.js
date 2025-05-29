f992be1fa05f3eac9a262dd49ce5a6c4a8a82621
Arpit Maurya <arpitbabu802@gmail.com>
2025-05-30 00:19:13 +0530

    Trigger redeploy with small update

--- Backend/server.js
+++ Backend/server.js
@@ -96,7 +96,7 @@ process.on('unhandledRejection', (err) => {
   }
 });
 
-// Start the server
+// Start the server 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
